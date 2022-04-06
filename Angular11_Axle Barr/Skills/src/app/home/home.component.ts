5a0599d5ea893415c6ad1d5f03f8e84ba1c9b98e
victor-mascarenas <victor-javier.garcia@atos.net>
2022-04-06 14:58:01 -0500

    Component styles
    Practice with component URL & inline styles

--- Angular11_Axle Barr/Skills/src/app/home/home.component.ts
+++ Angular11_Axle Barr/Skills/src/app/home/home.component.ts
@@ -2,11 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
   /* template: `
     <p>home works!</p>
     <h3>Hello</h3>
   `, */
+  templateUrl: './home.component.html',
+  /* styles: [
+    'div#demo {color: red;}',
+    'div#demo {border-style: solid;}',
+    'div#demo {border-color: grey;}',
+    'div#demo {width: 200px;}',
+    'div#demo {height: 40px;}',
+  ], */
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
