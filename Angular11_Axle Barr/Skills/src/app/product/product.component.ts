ea07797284c0a7ace8e0241d663d9587d465c016
victor-mascarenas <victor-javier.garcia@atos.net>
2022-05-04 16:46:22 -0500

    Shared modules
    Practice with shared modules

--- Angular11_Axle Barr/Skills/src/app/product/product.component.ts
+++ Angular11_Axle Barr/Skills/src/app/product/product.component.ts
@@ -2,9 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+    styleUrls: [
+        './product.component.css'
+    ]
 })
 
 export class ProductComponent{
-
+    showDiv = true;
 }
--- Angular11_Axle Barr/Skills/src/app/products-feature/products-feature.module.ts
+++ Angular11_Axle Barr/Skills/src/app/products-feature/products-feature.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../product/product.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductComponent
   ],
   imports: [
-    CommonModule
+    SharedModule
   ],
   exports: [
     ProductComponent
