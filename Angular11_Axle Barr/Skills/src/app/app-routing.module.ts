b189dba2b7934a7f619171743f370deb9576d385
victor-mascarenas <victor-javier.garcia@eviden.com>
2024-01-17 23:30:02 -0600

    Guards
    Allowing/preventing route access using guards

--- Angular11_Axle Barr/Skills/src/app/app-routing.module.ts
+++ Angular11_Axle Barr/Skills/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'product/:id', component: ProductComponent }
+  { path: 'product', canActivate: [AuthGuard], component: ProductComponent }
 ];
 
 @NgModule({
